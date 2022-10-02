0c5f0be295ba09f1c9756a3f688736422824edbe
lumitsudo <rm93909@fiap.com.br>
2022-10-02 18:18:37 -0300

    Correção useStates

--- react-app-pet/src/components/PetCadastro.jsx
+++ react-app-pet/src/components/PetCadastro.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DivInput } from '../style/styled'
 
 export default function PetCadastro() {
 
     const [pet, setPet] = useState({'nome':'', 'idade':'', 'raca':'', 'tamanho':'', 'nomeDoDono':'', 'telefoneDoDono':'', 'imagem':'', 'observacao':''})
+    const [listaPet, setListaPet] = useState([])
+
+    function inserirPet(e){
+        e.preventDefalut()
+
+        setListaPet([...listaPet, pet])
+        console.log(listaPet);
+    }
+
+    function cadPet(e){
+        setPet({...pet, [e.target.name]:e.target.value})
+    }
 
     return(
         <DivInput>
@@ -11,47 +23,61 @@ export default function PetCadastro() {
             <div>
                 <label><b>Nome</b></label>
                 <br />
-                <input type="text" name='nome' placeholder="Nome" onChange={} value={pet.nome}/>
+                <input type="text" name='nome' placeholder="Nome" onChange={cadPet} value={pet.nome}/>
             </div>
             <div>
                 <label><b>Idade</b></label>
                 <br />
-                <input type="number" name='idade' placeholder="Idade" onChange={} value={pet.idade}/>
+                <input type="number" name='idade' placeholder="Idade" onChange={cadPet} value={pet.idade}/>
             </div>
             <div>
                 <label><b>Raça</b></label>
                 <br />
-                <input type="text" name='raca' placeholder="Raça" onChange={} value={pet.raca}/>
+                <input type="text" name='raca' placeholder="Raça" onChange={cadPet} value={pet.raca}/>
             </div>
             <div>
                 <label><b>Tamanho em cm</b></label>
                 <br />
-                <input type="number" name='tamanho' placeholder="Tamanho" onChange={} value={pet.tamanho}/>
+                <input type="number" name='tamanho' placeholder="Tamanho" onChange={cadPet} value={pet.tamanho}/>
             </div>
             <div>
                 <label><b>Nome do dono</b></label>
                 <br />
-                <input type="text" name='nomeDoDono' placeholder="Nome Do Dono" onChange={} value={pet.nomeDoDono}/>
+                <input type="text" name='nomeDoDono' placeholder="Nome Do Dono" onChange={cadPet} value={pet.nomeDoDono}/>
             </div>
             <div>
                 <label><b>Telefone do dono</b></label>
                 <br />
-                <input type="text" name='telefoneDoDono' placeholder="Telefone Do Dono" onChange={} value={pet.telefoneDoDono}/>
+                <input type="text" name='telefoneDoDono' placeholder="Telefone Do Dono" onChange={cadPet} value={pet.telefoneDoDono}/>
             </div>
             <div>
                 <label><b>Imagem do pet</b></label>
                 <br />
-                <input type="file" accept="image/*" name='imagem' placeholder="Imagem" onChange={} value={pet.imagem}/>
+                <input type="file" accept="image/*" name='imagem' placeholder="Imagem" onChange={cadPet} value={pet.imagem}/>
             </div>
             <div>
                 <label><b>Observações</b></label>
                 <br />
-                <textarea cols="30" rows="10" name='observacao' placeholder="Observação" onChange={} value={pet.observacao}></textarea>
+                <textarea cols="30" rows="10" name='observacao' placeholder="Observação" onChange={cadPet} value={pet.observacao}></textarea>
             </div>
             <div>
                 <button type="submit">Adicionar</button>
             </div>
             </form>
+            <div>
+                {listaPet.map((pt, i)=>
+                <div className='etiqueta' key={i}>
+                    <p>Nome: {pt.nome}</p>
+                    <p>Idade: {pt.idade}</p>
+                    <p>Raça: {pt.raca}</p>
+                    <p>Tamanho: {pt.tamanho}</p>
+                    <p>Nome do Dono: {pt.nomeDoDono}</p>
+                    <p>Telefone do Dono: {pt.telefoneDoDono}</p>
+                    <p>Imagem do Pet: {pt.imagem}</p>
+                    <p>Observações: {pt.observacao}</p>
+                </div>
+                )}
+            </div>
         </DivInput>
     )
 }
